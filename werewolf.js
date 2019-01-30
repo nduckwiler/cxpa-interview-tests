de5ed6a215de12e426a95be2941db87e181b3476
Nick Duckwiler <nduckwiler@gmail.com>
2019-01-30 15:21:42 -0500

    Switch werewolf.js to intial code

--- werewolf.js
+++ werewolf.js
@@ -1,13 +1,5 @@
 /*
-Initial code:
-const moonPhase = 'full';
-
-if (moonPhase === 'full') {
-  console.log('Howwwlll!!');
-} else {
-  console.log('I swear I am not a werewolf...');
-}
-
+Instruction:
 We all know that turning into a werewolf is not an instant thing. It happens in stages. So let's expand our program from before to accommodate that fact.
 
 If the moon is `'mostly full'`, log `'Arms and legs are getting hairier'`.
@@ -15,15 +7,11 @@ If the moon is `'mostly new'`, log `'Back on two feet'`.
 If someone enters in an invalid moon phase, log `'Invalid moon phase'`.
 */
 
-// This code passes tests in test.js:
+// Initial code. This should fail.
+const moonPhase = 'full';
 
 if (moonPhase === 'full') {
-
-} else if (moonPhase === 'mostly full') {
-
-} else if (moonPhase === 'mostly new') {
-
+  console.log('Howwwlll!!');
 } else {
-  return 'Invalid';
-
+  console.log('I swear I am not a werewolf...');
 }
